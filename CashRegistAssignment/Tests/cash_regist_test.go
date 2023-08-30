76f337455cc1da419e8a7003fcd04927f8e7ce77
Seyma SARIGİL <seymasargil@gmail.com>
2023-08-30 11:08:32 +0300

    totalpriceempty func added

--- CashRegistAssignment/Tests/cash_regist_test.go
+++ CashRegistAssignment/Tests/cash_regist_test.go
@@ -1,17 +1,25 @@
 package main_test
 
 import (
-	"cashregister"
+	m "cashregister"
 	"testing"
 )
 
 func TestTotalPrice(t *testing.T) {
 	want := 15.00
-	item := []main.Describable{
-		main.Item{"Cips", 15.00, 0.00},
+	item := []m.Describable{
+		m.Item{"Cips", 15.00, 0.00},
 	}
-	got := main.TotalPrice(item)
+	got := m.TotalPrice(item)
 
+	if got != want {
+		t.Errorf("want: %v; got: %v", want, got)
+	}
+}
+
+func TestTotalPriceEmpty(t *testing.T) {
+	want := 0.0
+	got := m.TotalPrice([]m.Describable{})
 	if got != want {
 		t.Errorf("want: %v; got: %v", want, got)
 	}
